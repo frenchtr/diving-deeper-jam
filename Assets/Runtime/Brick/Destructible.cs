ae135e76724167f4c0ef5c72d72778d018155ae6
Travis French <travis.french.1991@gmail.com>
2023-08-20 18:08:29 -0700

    Made bricks only destructible when colliding with player from above.

--- Assets/Runtime/Brick/Destructible.cs
+++ Assets/Runtime/Brick/Destructible.cs
@@ -5,9 +5,21 @@ namespace OTStudios.DDJ.Runtime.Runtime.Brick
 {
     public class Destructible : MonoBehaviour
     {
-        private void OnCollisionEnter2D(Collision2D other)
+        private void OnCollisionEnter2D(Collision2D collision)
         {
-            Destroy(this.gameObject);
+            if (collision.gameObject.gameObject.name == "Player")
+            {
+                foreach (var contact in collision.contacts)
+                {
+                    var dot = Vector2.Dot(Vector2.up, contact.normal);
+                    Debug.Log(dot);
+                    
+                    if (dot < -0.8f)
+                    {
+                        Destroy(this.gameObject);
+                    }
+                }
+            }
         }
     }
 }
