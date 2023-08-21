1b00b2259746fd602405e2e88d19c10c39ff6293
Travis French <travis.french.1991@gmail.com>
2023-08-20 17:53:25 -0700

    Created brick prefab.

---
+++ Assets/Runtime/Brick/Destructible.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace OTStudios.DDJ.Runtime.Runtime.Brick
+{
+    public class Destructible : MonoBehaviour
+    {
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
